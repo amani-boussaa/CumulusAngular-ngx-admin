55d87cd1a99b66af1de27e08d3b5271a1228e1e1
ladhari-youssef <youssefladhari.r@gmail.com>
2023-05-08 14:04:38 +0100

    changed URL to use enviroment

--- src/app/Service/Thread.Service.ts
+++ src/app/Service/Thread.Service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Thread } from '../Entity/Thread';
 import { Comment } from '../Entity/Comment';
 import { SlideOutComponent } from '../pages/e-commerce/slide-out/slide-out.component';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +17,11 @@ export class ThreadService {
   createThread(thread: any){
     console.log("CREATE THREAD METHODE");
     console.log(thread);
-    return this.http.post<Thread>(`http://localhost:8081/CUMULUS/api/thread/createThread`, thread);
+    return this.http.post<Thread>(`${environment.urlBackend}api/thread/createThread`, thread);
   }
   createThreadWithTags(thread: any,tags:string){
     console.log(thread);
-    return this.http.post<Thread>(`http://localhost:8081/CUMULUS/api/thread/createThreadWithTags?tags=${tags}`, thread);
+    return this.http.post<Thread>(`${environment.urlBackend}api/thread/createThreadWithTags?tags=${tags}`, thread);
   }
 
   updateThread(id: number, thread: any) {
@@ -28,13 +29,13 @@ export class ThreadService {
   }
 
   getThreadById(id: number) {
-    return this.http.get<Thread>(`http://localhost:8081/CUMULUS/api/thread/getThreadById/${id}`);
+    return this.http.get<Thread>(`${environment.urlBackend}api/thread/getThreadById/${id}`);
   }
 
   getAllthreads(){
 
 
-    return this.http.get<Thread[]>(`http://localhost:8081/CUMULUS/api/thread/getAllThreads`);
+    return this.http.get<Thread[]>(`${environment.urlBackend}api/thread/getAllThreads`);
 
     // axios.get('http://localhost:8081/CUMULUS/api/thread/getAllThreads')
     // .then(response => {
@@ -51,22 +52,22 @@ export class ThreadService {
   }
   
   getThreadByName(name: string){
-    return this.http.get<Thread[]>(`http://localhost:8081/CUMULUS/api/threadtag/getThreadByName/${name}`);
+    return this.http.get<Thread[]>(`${environment.urlBackend}api/threadtag/getThreadByName/${name}`);
   }
   getThreadByUser(userId: number){
-    return this.http.get(`http://localhost:8081/CUMULUS/api/thread/getThreadByUser/${userId}`);
+    return this.http.get(`${environment.urlBackend}api/thread/getThreadByUser/${userId}`);
   }
   postComment(threadid: number,comment:Comment){
     console.log("postclicked Service");
     console.log(comment);
     
-    return this.http.post(`http://localhost:8081/CUMULUS/api/thread/addComment/${threadid}`,comment)  .subscribe(
+    return this.http.post(`${environment.urlBackend}api/thread/addComment/${threadid}`,comment)  .subscribe(
       response => console.log(response),
       error => console.log(error)
     );
   }
   deleteThread(id: number) {
-    return this.http.delete(`http://localhost:8081/CUMULUS/api/thread/delete/${id}`);
+    return this.http.delete(`${environment.urlBackend}api/thread/delete/${id}`);
   }
 
 
